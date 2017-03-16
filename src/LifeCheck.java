6746be20a1182c9e1e29ff069e80892c312668e0
igorriok <i.solonari@gmail.com>
2017-03-16 17:34:16 +0200

    check ships life

--- src/LifeCheck.java
+++ src/LifeCheck.java
@@ -1,8 +1,19 @@
 
 public class LifeCheck extends Thread {
+    
+    private ConcurrentHashMap<String, Ship> shipList;
+    
+    public LifeCheck(ConcurrentHashMap<String, Ship> shipList) {
+        this.shipList = shipList;
+    }
 
     public void run() {
-
+        while(true) {
+            shipList.forEach((k,v) -> if(v.getLife().compareTo(LocalDateTime.now()) > 60000) {
+                shipList.remove(k)
+            });
+            Thread.sleep(60000);
+        }
 
     }
 }
