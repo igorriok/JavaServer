928a81cd2df16364f11ea745c85c24832d79a538
igorriok <i.solonari@gmail.com>
2017-04-13 11:56:13 +0300

    missle

--- src/Server/Missles.java
+++ src/Server/Missles.java
@@ -6,33 +6,15 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Missles extends Thread {
 
-    private ConcurrentHashMap<String, Ship> missleList;
+    private ConcurrentLinkedQueue<Ship> missleList;
 
 
-    protected void addMissle(int ID, Ship location) {
-        if(shipList.containsKey(ID)) {
-            shipList.replace(ID, location);
-        } else {
-            shipList.put(ID, location);
-        }
-    }
-
-    protected void removeShip(String ID) {
-        if(shipList.containsKey(ID)) {
-            shipList.remove(shipList.get(ID));
-        }
+    protected void addMissle(Ship ship) {
+        missleList.add(ship);
     }
     
-    protected ConcurrentHashMap getShips() {
-        return shipList;
+    protected ConcurrentLinkedQueue getMissles() {
+        return missleList;
     }
-
-    public void run(){
-        shipList = new ConcurrentHashMap<String, Ship>();
-        shipList.put("north", new Ship(47.5, 29, LocalTime.now()));
-        shipList.put("south", new Ship(46.5, 29, LocalTime.now()));
-        shipList.put("west", new Ship(47, 28.1, LocalTime.now()));
-        shipList.put("est", new Ship(47, 29.6, LocalTime.now()));
-    }
-
+    
 }
